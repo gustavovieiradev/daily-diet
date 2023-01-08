c421102e85104ebfa98fecd4eba7ec4aebe04ad2
Gustavo Vieira <gustavovieiradev@gmail.com>
2023-01-08 11:43:37 -0300

    feat: create theme colors/size/fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold
+} from '@expo-google-fonts/nunito';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
   return (
+    
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
