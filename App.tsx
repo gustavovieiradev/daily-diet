d39da26573efa5634b2e143a25b6abc0448eed6e
Gustavo Vieira <gustavovieiradev@gmail.com>
2023-01-08 11:54:27 -0300

    feat: add styled components

--- App.tsx
+++ App.tsx
@@ -3,17 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import {
   useFonts,
   Nunito_400Regular,
-  Nunito_700Bold
+  Nunito_700Bold,
 } from '@expo-google-fonts/nunito';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
   return (
-    
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
