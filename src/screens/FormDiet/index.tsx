052cc4526676eabe8c35d3c3ee5de5259187f934
Gustavo Vieira <gustavovieiradev@gmail.com>
2023-03-04 18:39:20 -0300

    feat(new): show component feedback after save

--- src/screens/FormDiet/index.tsx
+++ src/screens/FormDiet/index.tsx
@@ -4,6 +4,7 @@ import { Alert, View } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import BackButton from '../../components/BackButton';
 import { Button } from '../../components/Button';
+import Feedback from '../../components/Feedback';
 import { Input } from '../../components/Input';
 import { Select } from '../../components/Select';
 import Space from '../../components/Space';
@@ -25,9 +26,6 @@ const FormDiet: React.FC = () => {
   // hook theme
   const theme = useTheme();
 
-  // hook navigation
-  const navigation = useNavigation();
-
   // hook state status diet
   const [statusDiet, setStatuDiet] = useState<ValueStatus>('on');
   // hook state name diet
@@ -38,6 +36,8 @@ const FormDiet: React.FC = () => {
   const [date, setDate] = useState('');
   // hook state hour diet
   const [hour, setHour] = useState('');
+  // hook state show feedback save
+  const [finishSave, setFinishSave] = useState(false);
 
   // action set status diet
   function handleSetStatus(value: ValueStatus) {
@@ -55,56 +55,60 @@ const FormDiet: React.FC = () => {
         status: statusDiet,
       });
 
-      Alert.alert('Refeição cadastrada com sucesso!');
-
-      navigation.navigate('detail');
+      setFinishSave(true);
     } catch (err) {
       Alert.alert('Preencha todos os campos de forma correta!');
     }
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton color={theme.COLORS.GRAY_100} />
-        <HeaderTitle>Nova refeição</HeaderTitle>
-      </Header>
-      <Content>
-        <Input label="Nome" onChangeText={setName} />
-        <Input label="Descrição" onChangeText={setDescription} multiline />
-        <Row>
-          <Column>
-            <Input label="Data" onChangeText={setDate} />
-          </Column>
-          <Space width={10} />
-          <Column>
-            <Input label="Hora" onChangeText={setHour} />
-          </Column>
-        </Row>
+    <>
+      {finishSave ? (
+        <Feedback status={statusDiet} />
+      ) : (
+        <Container>
+          <Header>
+            <BackButton color={theme.COLORS.GRAY_100} />
+            <HeaderTitle>Nova refeição</HeaderTitle>
+          </Header>
+          <Content>
+            <Input label="Nome" onChangeText={setName} />
+            <Input label="Descrição" onChangeText={setDescription} multiline />
+            <Row>
+              <Column>
+                <Input label="Data" onChangeText={setDate} />
+              </Column>
+              <Space width={10} />
+              <Column>
+                <Input label="Hora" onChangeText={setHour} />
+              </Column>
+            </Row>
 
-        <Label>Está dentro da dieta?</Label>
-        <Row>
-          <Select
-            title="Sim"
-            active={statusDiet === 'on'}
-            type="success"
-            status="on"
-            onPress={() => handleSetStatus('on')}
-          />
-          <Space width={10} />
-          <Select
-            title="Não"
-            active={statusDiet === 'off'}
-            type="danger"
-            status="off"
-            onPress={() => handleSetStatus('off')}
-          />
-        </Row>
-      </Content>
-      <Action>
-        <Button title="Cadastrar refeição" onPress={handleSave} />
-      </Action>
-    </Container>
+            <Label>Está dentro da dieta?</Label>
+            <Row>
+              <Select
+                title="Sim"
+                active={statusDiet === 'on'}
+                type="success"
+                status="on"
+                onPress={() => handleSetStatus('on')}
+              />
+              <Space width={10} />
+              <Select
+                title="Não"
+                active={statusDiet === 'off'}
+                type="danger"
+                status="off"
+                onPress={() => handleSetStatus('off')}
+              />
+            </Row>
+          </Content>
+          <Action>
+            <Button title="Cadastrar refeição" onPress={handleSave} />
+          </Action>
+        </Container>
+      )}
+    </>
   );
 };
 
